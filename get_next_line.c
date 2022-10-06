fd8d93d73dc8130d00452780878cddf1705bcb7f
feli-bar <feli-bar@student.42sp.org.br>
2022-10-06 21:42:20 +0200

    Adicionando exercicios revisados para testar em casa

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: feli-bar <feli-bar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 16:26:14 by feli-bar          #+#    #+#             */
-/*   Updated: 2022/10/06 19:47:42 by feli-bar         ###   ########.fr       */
+/*   Updated: 2022/10/06 21:40:43 by feli-bar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,15 @@ char	*ft_check_first_line(char *str)
 	i = 0;
 	if(str[i] == '\0')
 		return (NULL);
-	while (str[i] != '\0' && str[i] != '\n')
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
 		i++;
 	strcheck = malloc(sizeof(char) * (i + str[i] == '\n' + 1));
 	if (!strcheck)
 		return (NULL);
 	i = 0;
-	while (str[i] != '\0' && str[i] != '\n')
+	while (str[i] && str[i] != '\n')
 	{
 		strcheck[i] = str[i];
 		i++;
@@ -48,74 +50,69 @@ char	*ft_check_next_line(char *str)
 	size_t	i;
 	size_t	j;
 
-	i = 0; 
-	while (str[i] != '\0' && str[i] != '\n')
+	i = 0;
+	j = 0;
+	if (!str)
+		return (NULL); 
+	while (str[i] != '\n' && str[i] != '\0')
 		i++;	
-	newstr = malloc(sizeof(char) * ft_strlen(str) - i + 1);
-	if (!newstr)
-		return (NULL);
 	if (str[i] == '\n')
 		i++;
-	j = 0;
-	while (str[i + j] != '\0')
+	newstr = malloc(sizeof(char) * (ft_strlen(str) - i + 1));
+	if (!newstr)
+		return (NULL);
+	while (str[i + j])
 	{
 		newstr[j] = str[i + j];
 		j++;
 	}			
 	newstr[j] = '\0';			
-	if (str)
-		free (str);
+	free (str);
 	return (newstr);
 }
 
 char	*ft_read_line(int fd, char *str)
 {
 	char 	*buf;
-	int		t; 
+	int		i; 
 	
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (buf == NULL)
+	if (!buf)
 		return (NULL);
 	while (!ft_strchr(str, '\n'))
 	{
-		t = read (fd, buf, BUFFER_SIZE);
-		if (t <= 0)
+		i = read (fd, buf, BUFFER_SIZE);
+		if (i <= 0)
 		{
 			free (buf);
 			return (NULL);
 		}
-		buf[t] = '\0';
+		buf[i] = '\0';
 		str = ft_strjoin(str, buf);
-		if (str == NULL)
+		if (!str)
 		{
 			free (buf);
 			return (NULL);
 		}		
 	}
 	free (buf);
-	if (t == -1)
-	{
-		free (str);
-		return (NULL);
-	}
 	return (str);
 }
 
 char	*get_next_line(int fd)
 {
-	char	*str;
-	static char	*result;
+	static char *finalstr;
+	char	*str_read;
 	
-	str = "\0";
+	str_read = "\0";
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	result = ft_read_line(fd, result);
-	if (result == NULL)
+	finalstr = ft_read_line(fd, finalstr);
+	if (!finalstr)
 		return (NULL);
-	str = ft_check_first_line(result);
-	result = ft_check_next_line(result);
-	if (!result)
+	str_read = ft_check_first_line(finalstr);
+	finalstr = ft_check_next_line(finalstr);
+	if (!finalstr)
 		return (NULL);
-	return (str);
+	return (str_read);
 }
-
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: feli-bar <feli-bar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 16:26:41 by feli-bar          #+#    #+#             */
-/*   Updated: 2022/10/06 19:06:40 by feli-bar         ###   ########.fr       */
+/*   Updated: 2022/10/06 21:22:16 by feli-bar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,94 +21,120 @@ size_t	ft_strlen(char *str)
 	{
 		while (str[i] != '\0')
 			i++;
+		return (i);
 	}
-	return (i);
+	return (0);
 }
 char	*ft_strchr(char *str, int c)
 {
 	int	i;
 
 	i = 0;
-	if (str)
-	{
-		while (str[i] != '\0')
+	if (!str)
+		return (NULL);
+	while (str[i] != '\0')
 		{
 			if (str[i] == (char) c)
 				return ((char *) str + i);
 			i++;
 		}
-		if (c == '\0')
-			return ((char *) str + i);
-		return (0);
-	}
-	return (0);
+	return (NULL);
 }
 
-size_t	ft_strlcat(char *dst, char *src, size_t dstsize)
-{
-	size_t	src_len;
-	size_t	dst_len;
-	size_t	i;
+// size_t	ft_strlcat(char *dst, char *src, size_t dstsize)
+// {
+// 	size_t	src_len;
+// 	size_t	dst_len;
+// 	size_t	i;
 
-	src_len = ft_strlen(src);
-	dst_len = ft_strlen(dst);
-	i = 0;
-	if (dstsize <= dst_len)
-		return (src_len + dstsize);
-	while (src[i] != '\0' && i < (dstsize - dst_len - 1))
-	{
-		dst[dst_len + i] = src[i];
-		i++;
-	}
-	dst[dst_len + i] = '\0';
-	return (dst_len + src_len);
-}
+// 	src_len = ft_strlen(src);
+// 	dst_len = ft_strlen(dst);
+// 	i = 0;
+// 	if (dstsize <= dst_len)
+// 		return (src_len + dstsize);
+// 	while (src[i] != '\0' && i < (dstsize - dst_len - 1))
+// 	{
+// 		dst[dst_len + i] = src[i];
+// 		i++;
+// 	}
+// 	dst[dst_len + i] = '\0';
+// 	return (dst_len + src_len);
+// }
 
-size_t	ft_strlcpy(char *dst, char *src, size_t dstsize)
-{
-	size_t	i;
+// size_t	ft_strlcpy(char *dst, char *src, size_t dstsize)
+// {
+// 	size_t	i;
 	
-	if (dstsize)
-	{
-		i = 0;
-		while (src[i] != '\0' && i < dstsize -1)
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		//dst[i] = " ";
-	}	
-	return (ft_strlen(src));	
-}
+// 	i = 0;
+// 	if (dstsize <= 0)
+// 	{
+// 		return (ft_strlen(src));
+// 	}
+// 	while (src[i] != '\0' && i < dstsize -1)
+// 	{
+// 		((char *)dst)[i] = ((char *)src)[i];
+// 		i++;
+// 	}
+// 	dst[i] = '\0';
+// 	return (ft_strlen(src));	
+// }
 
-char	*ft_strdup(char *src)
-{
-	char	*dst;
-	size_t	len;
+// char	*ft_strdup(char *src)
+// {
+// 	char	*dst;
+// 	size_t	len;
 
-	len = ft_strlen(src) + 1;
-	dst = malloc(len);
-	if (dst)
-		ft_strlcpy(dst, src, len);
-	return (dst);
-}
+// 	len = ft_strlen(src) + 1;
+// 	dst = malloc(len);
+// 	if (dst)
+// 		ft_strlcpy(dst, src, len);
+// 	return (dst);
+// }
 
+// char	*ft_strjoin(char *str1, char *str2)
+// {
+// 	char	*finalstr;
+// 	size_t 	size;
+	
+// 	size = ft_strlen(str1) + ft_strlen(str2) + 1;
+// 	finalstr = malloc(size);
+// 	if (!str1 && !str2)
+// 		return (NULL);
+// 	if (!str1)
+// 		return (ft_strdup(str2));
+// 	if (!str2)
+// 		return (ft_strdup(str1));
+// 	if (!finalstr)
+// 		return (NULL);	
+// 	ft_strlcpy(finalstr, str1, size);
+// 	ft_strlcat (finalstr, str2, size);
+// 	return (finalstr);
+// }
 char	*ft_strjoin(char *str1, char *str2)
 {
-	char	*finalstr;
-	size_t 	size;
-	
-	size = ft_strlen(str1) + ft_strlen(str2) + 1;
-	finalstr = malloc(size);
-	if (!str1 && !str2)
+	char	*newstr;
+	size_t	len1;
+	size_t	len2;
+
+	len1 = 0;
+	len2 = 0;
+	newstr = (char *) malloc((ft_strlen(str1) + ft_strlen(str2) + 1));
+	if (!newstr)
 		return (NULL);
-	if (!str1)
-		return (ft_strdup(str2));
-	if (!str2)
-		return (ft_strdup(str1));
-	if (!finalstr)
-		return (NULL);	
-	ft_strlcpy(finalstr, str1, size);
-	ft_strlcat (finalstr, str2, size);
-	return (finalstr);
+	if (str1)
+	{
+		while (str1[len1])
+		{
+			newstr[len1] = str1[len1];
+			len1++;
+		}
+	}
+	while (str2[len2])
+	{
+		newstr[len1 + len2] = str2[len2];
+		len2++;
+	}	
+	newstr[len1 + len2] = '\0';
+	free (str1);
+	return (newstr);
 }
